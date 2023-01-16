b38dc94e69092ab86c078b01aca3c8601b1d5364
Peter Bull <rogueplanetoid@hotmail.com>
2023-01-16 17:10:01 +0000

    Add Set Nine

---
+++ Code/LuckyBunny/LuckyBunny/Library.cs
@@ -0,0 +1,173 @@
+﻿using Comentsys.Assets.FluentEmoji;
+using Comentsys.Toolkit.WindowsAppSdk;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Library
+{
+    private const string title = "Lucky Bunny";
+    private const int size = 5;
+    private const int bunnies = 3;
+    private const int maximum = 25;
+    private const double timer = 1;
+    private const bool bunny = true;
+    private const bool carrot = false;
+
+    private readonly Random _random = new((int)DateTime.UtcNow.Ticks);
+
+    private DispatcherTimer _timer;
+    private Dialog _dialog;
+    private Panel _panel;
+    private Grid _grid;
+
+    private List<int> _numbers;
+    private int _current;
+    private int _missed;
+    private bool _over;
+
+    private List<int> Choose(int minimum, int maximum, int total) =>
+        Enumerable.Range(minimum, maximum)
+            .OrderBy(r => _random.Next(minimum, maximum))
+                .Take(total).ToList();
+
+    // Get Asset, Get Content, Set Content, Over & Miss
+    private Asset GetAsset(bool? isBunny = null) =>
+    new()
+    {
+        AssetResource = FlatFluentEmoji.Get(
+        isBunny switch
+        {
+            bunny => FluentEmojiType.RabbitFace,
+            carrot => FluentEmojiType.Carrot,
+            _ => FluentEmojiType.None
+        })
+    };
+
+    private Asset GetContent(int counter) =>
+        _numbers[_current] == counter ? GetAsset(false) : GetAsset();
+
+    private void SetContent()
+    {
+        foreach (var button in _grid.Children.Cast<Button>())
+        {
+            button.Content = GetContent((int)button.Tag);
+        }
+    }
+
+    private void Over()
+    {
+        if (_over)
+            _dialog.Show("Game Over!");
+    }
+
+    private void Miss()
+    {
+        if (!_over)
+        {
+            _panel.Children.Remove(_panel.Children.FirstOrDefault());
+            _panel.Children.Add(GetAsset());
+            _missed++;
+            if (_missed >= bunnies)
+            {
+                _timer.Stop();
+                _over = true;
+            }
+        }
+        Over();
+    }
+
+    // Add
+    private void Add(Grid grid, int row, int column, int index)
+    {
+        Button button = new()
+        {
+            Width = 50,
+            Height = 50,
+            Tag = index,
+            Content = GetContent(index)
+        };
+        button.Click += (object sender, RoutedEventArgs e) =>
+        {
+            if (!_over)
+            {
+                if (_current < maximum - 1)
+                {
+                    var value = (int)button.Tag;
+                    if (_numbers[_current] == value)
+                    {
+                        _current++;
+                        SetContent();
+                        _timer.Start();
+                    }
+                    else
+                    {
+                        Miss();
+                    }
+                }
+                else
+                {
+                    _dialog.Show($"You Won with {_missed} missed!");
+                    _timer.Stop();
+                }
+            }
+            Over();
+        };
+        button.SetValue(Grid.ColumnProperty, column);
+        button.SetValue(Grid.RowProperty, row);
+        grid.Children.Add(button);
+    }
+
+    // Layout & New
+    private void Layout(Grid grid)
+    {
+        int index = 0;
+        grid.Children.Clear();
+        var panel = new StackPanel()
+        {
+            Orientation = Orientation.Vertical
+        };
+        _grid = new Grid();
+        for (int row = 0; row < size; row++)
+        {
+            _grid.RowDefinitions.Add(new RowDefinition());
+            for (int column = 0; column < size; column++)
+            {
+                if (row == 0)
+                    _grid.ColumnDefinitions.Add(new ColumnDefinition());
+                Add(_grid, row, column, index);
+                index++;
+            }
+        }
+        panel.Children.Add(_grid);
+        _panel = new StackPanel()
+        {
+            Orientation = Orientation.Horizontal,
+            HorizontalAlignment = HorizontalAlignment.Center
+        };
+        for (int counter = 0; counter < bunnies; counter++)
+        {
+            _panel.Children.Add(GetAsset(true));
+        }
+        panel.Children.Add(_panel);
+        grid.Children.Add(panel);
+    }
+
+    public void New(Grid grid)
+    {
+        _dialog = new Dialog(grid.XamlRoot, title);
+        _numbers = Choose(0, size * size, maximum);
+        _timer = new()
+        {
+            Interval = TimeSpan.FromSeconds(timer)
+        };
+        _timer.Tick += (object sender, object e) =>
+            Miss();
+        _over = false;
+        _current = 0;
+        _missed = 0;
+        Layout(grid);
+    }
+}
---
+++ Code/LuckyRacer/LuckyRacer/Library.cs
@@ -0,0 +1,291 @@
+﻿using Comentsys.Assets.FluentEmoji;
+using Comentsys.Toolkit.WindowsAppSdk;
+using Microsoft.UI;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Animation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace LuckyRacer;
+
+public enum State
+{
+    Select, Ready, Started, Finished
+}
+
+public class Racer
+{
+    public int Index { get; set; }
+    public TimeSpan Time { get; set; }
+
+    public Racer(int index) =>
+        Index = index;
+
+    public Racer(int index, TimeSpan time) =>
+        (Index, Time) = (index, time);
+}
+
+public class Library
+{
+    // Constants, Variables & Choose Method
+    private const string title = "Lucky Racer";
+    private const int image_size = 72;
+    private const int size = 400;
+
+    private readonly Random _random = new((int)DateTime.UtcNow.Ticks);
+
+    private Dialog _dialog;
+    private Grid _grid;
+
+    private bool _finish;
+    private int _count;
+    private State _state;
+    private Racer _winner;
+    private Racer _select;
+    private List<Image> _images;
+    private ImageSource[] _sources;
+
+    private List<int> Choose(int minimum, int maximum, int total)
+    {
+        var choose = new List<int>();
+        var values = Enumerable.Range(minimum, maximum).ToList();
+        for (int index = 0; index < total; index++)
+        {
+            var value = _random.Next(0, values.Count);
+            choose.Add(values[value]);
+        }
+        return choose;
+    }
+
+    // Get Finish, Get Racer, Set Sources & Get Image
+
+    private async Task<ImageSource> GetFinishAsync() =>
+        await FlatFluentEmoji.Get(FluentEmojiType.ChequeredFlag)
+        .AsImageSourceAsync();
+
+    private async Task<ImageSource> GetRacerAsync(Color main, Color trim) =>
+        await FlatFluentEmoji.Get(FluentEmojiType.RacingCar,
+        new[]
+        {
+        Color.FromArgb(255, 248, 49, 47).AsDrawingColor(),
+        Color.FromArgb(255, 202, 11, 74).AsDrawingColor()
+        },
+        new[]
+        {
+        main.AsDrawingColor(),
+        trim.AsDrawingColor()
+        }).AsImageSourceAsync();
+
+    private async Task SetSourcesAsync() =>
+    _sources ??= (new ImageSource[]
+    {
+    await GetFinishAsync(),
+    await GetRacerAsync(Colors.Red, Colors.DarkRed),
+    await GetRacerAsync(Colors.Blue, Colors.DarkBlue),
+    await GetRacerAsync(Colors.Green, Colors.DarkGreen),
+    await GetRacerAsync(Colors.Goldenrod, Colors.DarkGoldenrod)
+    });
+
+    private Image GetImage(ImageSource source) =>
+        new()
+        {
+            Height = image_size,
+            Width = image_size,
+            Source = source
+        };
+
+    // Content, Move & Start
+    private StackPanel Content(string text, int index)
+    {
+        var panel = new StackPanel()
+        {
+            Orientation = Orientation.Vertical,
+        };
+        panel.Children.Add(new TextBlock()
+        {
+            Text = text
+        });
+        panel.Children.Add(GetImage(_sources[index]));
+        return panel;
+    }
+
+    private void Move(Image image, double from, double to, TimeSpan duration)
+    {
+        var animation = new DoubleAnimation()
+        {
+            To = to,
+            From = from,
+            Duration = duration,
+            EasingFunction = new ExponentialEase()
+            {
+                EasingMode = EasingMode.EaseIn
+            }
+        };
+        var storyboard = new Storyboard();
+        Storyboard.SetTargetProperty(animation, "(Canvas.Left)");
+        Storyboard.SetTarget(animation, image);
+        storyboard.Completed += (object sender, object e) =>
+            Progress(sender as Storyboard);
+        storyboard.Children.Add(animation);
+        storyboard.Begin();
+    }
+
+    private void Start()
+    {
+        _count = 0;
+        _finish = false;
+        _state = State.Select;
+    }
+
+    // Finish & Progress
+    private async void Finish()
+    {
+        if (_state == State.Finished)
+        {
+            var message = _select.Index == _winner.Index ?
+                $"You Won in {_winner.Time}!" :
+                $"You Lost! Winning Car";
+            var content = Content(message, _winner.Index);
+            await _dialog.ConfirmAsync(content);
+            if (_finish)
+            {
+                foreach (var image in _images)
+                {
+                    Move(image, 0, size - image_size,
+                        TimeSpan.FromSeconds(1));
+                }
+                _finish = false;
+            }
+            Start();
+        }
+    }
+
+    private void Progress(Storyboard storyboard)
+    {
+        if (_state == State.Started)
+        {
+            var duration = storyboard.GetCurrentTime();
+            var racer = _images.First(w => (w.Tag as Racer)
+                .Time == duration).Tag as Racer;
+            _count++;
+            if (_count == 1)
+                _winner = new Racer(racer.Index, duration);
+            if (_count == _images.Count)
+            {
+                _state = State.Finished;
+                Finish();
+            }
+            _finish = true;
+        }
+    }
+
+    // Race, Ready & Select
+    private void Race()
+    {
+        if (_state == State.Ready)
+        {
+            var index = 0;
+            var times = Choose(5, 15, _sources.Length - 1);
+            foreach (var image in _images)
+            {
+                var racer = image.Tag as Racer;
+                racer.Time = TimeSpan.FromSeconds(times[index]);
+                Move(image, size - image_size, 0, racer.Time);
+                index++;
+            }
+            _state = State.Started;
+        }
+    }
+
+    private async void Ready()
+    {
+        if (_state == State.Ready)
+        {
+            var content = Content("Selected to Win", _select.Index);
+            var result = await _dialog.ConfirmAsync(
+                content, "Race", "Cancel");
+            if (result)
+                Race();
+            else
+                _state = State.Select;
+        }
+    }
+
+    private void Select(Image image)
+    {
+        if (_state == State.Select)
+        {
+            var racer = image.Tag as Racer;
+            _select = racer;
+            _state = State.Ready;
+        }
+        Ready();
+    }
+
+    // Add Racer & Add Finish
+
+    private void AddRacer(Grid grid, int row)
+    {
+        grid.RowDefinitions.Add(new RowDefinition());
+        var racer = GetImage(_sources[row]);
+        racer.Tag = new Racer(row);
+        racer.Tapped += (object sender, TappedRoutedEventArgs e) =>
+            Select(sender as Image);
+        Canvas.SetLeft(racer, size - image_size);
+        _images.Add(racer);
+        var canvas = new Canvas()
+        {
+            Height = image_size,
+            Width = size
+        };
+        canvas.Children.Add(racer);
+        Grid.SetRow(canvas, row - 1);
+        grid.Children.Add(canvas);
+    }
+
+    private void AddFinish(Grid grid, int row)
+    {
+        grid.RowDefinitions.Add(new RowDefinition());
+        var finish = GetImage(_sources.First());
+        Grid.SetRow(finish, row - 1);
+        grid.Children.Add(finish);
+    }
+
+    // Layout & New
+    private void Layout(Grid grid)
+    {
+        _images = new();
+        grid.Children.Clear();
+        var panel = new StackPanel()
+        {
+            Orientation = Orientation.Horizontal
+        };
+        _grid = new Grid()
+        {
+            Height = size,
+            Width = size
+        };
+        var finish = new Grid();
+        for (int row = 1; row < _sources.Length; row++)
+        {
+            AddRacer(_grid, row);
+            AddFinish(finish, row);
+        }
+        panel.Children.Add(finish);
+        panel.Children.Add(_grid);
+        grid.Children.Add(panel);
+    }
+
+    public async void New(Grid grid)
+    {
+        _dialog = new Dialog(grid.XamlRoot, title);
+        await SetSourcesAsync();
+        Layout(grid);
+        Start();
+    }
+}
