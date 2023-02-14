9ff1f5230c4fe0dcfdd8822877048d019a6172ac
Peter Bull <rogueplanetoid@hotmail.com>
2023-02-14 12:01:40 +0000

    Add Set Thirteen Part One

---
+++ Code/DonutControl/DonutControl/Library.cs
@@ -0,0 +1,150 @@
+﻿using Comentsys.Toolkit.WindowsAppSdk;
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.Devices.Radios;
+using Windows.UI;
+
+namespace DonutControl;
+
+public class Donut : Grid
+{
+    private const double total = 100;
+    private const double circle = 360;
+
+    private List<double> _items = new();
+
+    // Donut GetSector & Percentages Method
+    private static Sector GetSector(double size, double start,
+    double finish, double radius, double hole, Color fill)
+    {
+        Sector sector = new()
+        {
+            Hole = hole,
+            Start = start,
+            Finish = finish,
+            Radius = radius,
+            Fill = new SolidColorBrush(fill),
+            Stroke = new SolidColorBrush(Colors.WhiteSmoke)
+        };
+        Canvas.SetLeft(sector, (size - radius * 2) / 2);
+        Canvas.SetTop(sector, (size - radius * 2) / 2);
+        return sector;
+    }
+
+    private List<double> Percentages()
+    {
+        List<double> results = new();
+        double total = _items.Sum();
+        foreach (double item in _items)
+        {
+            results.Add(item / total * 100);
+        }
+        return results.OrderBy(o => o).ToList();
+    }
+
+    // Donut Layout Method
+    internal void Layout()
+    {
+        double finish = 0;
+        double value = circle / total;
+        List<double> percentages = Percentages();
+        Canvas canvas = new()
+        {
+            Width = Radius * 2,
+            Height = Radius * 2
+        };
+        Children.Clear();
+        for (int index = 0; index < percentages.Count; index++)
+        {
+
+            double start = finish;
+            double percentage = percentages[index];
+            Color colour = (index < Palette.Count) ? Palette[index] : Colors.Black;
+            double sweep = value * percentage;
+            finish = sweep + start;
+            if (finish >= 360)
+                finish = sweep;
+            Sector sector = GetSector(Radius * 2, start, finish, Radius, Hole, colour);
+            canvas.Children.Add(sector);
+        }
+        Viewbox viewbox = new()
+        {
+            Child = canvas
+        };
+        Children.Add(viewbox);
+    }
+
+    // Donut Properties
+    public List<Color> Palette { get; set; } = new();
+
+    public List<double> Items
+    {
+        get { return _items; }
+        set { _items = value; Layout(); }
+    }
+
+    public static readonly DependencyProperty RadiusProperty =
+    DependencyProperty.Register("Radius", typeof(int),
+    typeof(Donut), new PropertyMetadata(100, new PropertyChangedCallback(
+    (DependencyObject obj, DependencyPropertyChangedEventArgs eventArgs) =>
+    {
+        ((Donut)obj).Layout();
+    })));
+
+    public static readonly DependencyProperty HoleProperty =
+    DependencyProperty.Register("Hole", typeof(UIElement),
+    typeof(Donut), new PropertyMetadata(50.0, new PropertyChangedCallback(
+    (DependencyObject obj, DependencyPropertyChangedEventArgs eventArgs) =>
+    {
+        ((Donut)obj).Layout();
+    })));
+
+    public int Radius
+    {
+        get { return (int)GetValue(RadiusProperty); }
+        set { SetValue(RadiusProperty, value); Layout(); }
+    }
+
+    public double Hole
+    {
+        get { return (double)GetValue(HoleProperty); }
+        set { SetValue(HoleProperty, value); Layout(); }
+    }
+}
+
+public class Library
+{
+    private readonly List<Color> _colours = new()
+    {
+        Colors.Black,
+        Colors.Gray,
+        Colors.Red,
+        Colors.Orange,
+        Colors.Yellow,
+        Colors.Green,
+        Colors.Cyan,
+        Colors.Blue,
+        Colors.Magenta,
+        Colors.Purple
+    };
+
+    // Library Methods 
+    private int Fibonacci(int value) => value > 1 ?
+    Fibonacci(value - 1) + Fibonacci(value - 2) : value;
+
+    public void Load(Grid grid)
+    {
+        grid.Children.Clear();
+        Donut donut = new()
+        {
+            Palette = _colours
+        };
+        donut.Items = Enumerable.Range(1, donut.Palette.Count)
+        .Select(Fibonacci).Select(s => (double)s).ToList();
+        grid.Children.Add(donut);
+    }
+}
---
+++ Code/LightControl/LightControl/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace LightControl
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private readonly Library _library = new();
+
+        private void Load(object sender, RoutedEventArgs e) =>
+            _library.Load(Display);
+
+        private void Play(object sender, RoutedEventArgs e) =>
+            _library.Traffic();
+
+        private void New(object sender, RoutedEventArgs e) =>
+            _library.Reset();
+
+    }
+}
